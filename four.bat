4da59bf136bf0ed9cc3f6f4d0eb4760c6e5eb4c1
IanWang <gogoenwon@gmail.com>
2013-10-30 12:32:51 +0800

    auto push test

--- four.bat
+++ four.bat
@@ -7,7 +7,7 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 echo Uploading blogs...
 git add .
 @ping 127.0.0.1 -n 3 -w 1000 > nul
-git commit -m 'auto push test'
+git commit -m "auto push test"
 @ping 127.0.0.1 -n 3 -w 1000 > nul
 git push
 @ping 127.0.0.1 -n 3 -w 5000 > nul
