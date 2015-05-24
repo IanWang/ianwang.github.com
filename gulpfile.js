042e32bf2b38fb55689131ac6a71f38a20cabfa3
Ian Wang <gogoenwon@gmail.com>
2015-05-24 17:26:21 +0800

    optimize assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var watcher = gulp.watch('./templates/*.hbs', ['default']);
 
 
@@ -9,7 +12,30 @@ watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
 
-gulp.task('default', function (){
+
+gulp.task('cssmin', function () {
+  return gulp.src([
+      'css/normalize.css',
+      'css/helpers.css',
+      'css/skeleton.css',
+      'css/style.css'
+    ])
+    .pipe(concat('style.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('imagemin', function () {
+  return gulp.src('img/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', function (){
   var templateData = require('./templates/projects.json');
   var options = {
     ignorePartials: true, 
@@ -21,3 +47,5 @@ gulp.task('default', function (){
     .pipe(rename('index.html'))
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['cssmin', 'imagemin', 'build'])
