436b7fe68f243252a301ae770c42e8a6cff2d048
Ian Wang <gogoenwon@gmail.com>
2018-07-15 21:37:39 +0800

    simplify content; fix ssl issue

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,11 @@ var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
 var watcher = gulp.watch('./templates/*.hbs', ['default']);
-
 
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
-
 
 gulp.task('cssmin', function () {
   return gulp.src([
@@ -25,20 +21,10 @@ gulp.task('cssmin', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('imagemin', function () {
-  return gulp.src('img/*')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('dist/img'));
-});
-
 gulp.task('build', function (){
   var templateData = require('./templates/projects.json');
   var options = {
-    ignorePartials: true, 
+    ignorePartials: true,
     batch: './templates' // filepath of partials
   };
 
@@ -48,4 +34,4 @@ gulp.task('build', function (){
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['cssmin', 'imagemin', 'build'])
+gulp.task('default', ['cssmin', 'build'])
